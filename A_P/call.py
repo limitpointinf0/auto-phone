e9228282b8b084d6fd6225070cfa2ce26e431d55
Christopher ONeal <coneal0523@protonmail.com>
2018-05-06 19:45:04 -0700

    Update call.py
    Modified script to call all items on calling list with arbitrary iterations and arbitrary time interval between calls.

--- A_P/call.py
+++ A_P/call.py
@@ -1,22 +1,38 @@
-# Download the Python helper library from twilio.com/docs/python/install
-from twilio.rest import Client
+import datetime
+import traceback
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "XXXXXXXXXXXXXXXXXX"
-auth_token = "XXXXXXXXXXXXXXXXXX"
+account_sid = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+phone_no = "XXXXXXXXXXXX"
 client = Client(account_sid, auth_token)
 
-with open('url.txt','r') as g:
-    URL = g.read()
-GROUP=input('Group: ')
+def call_them(start_time, call_list, interval=300, times=5):
+    global phone_no
+    for t in range(times):
+        try:
+            for i in call_list:
+                call = client.calls.create(
+                    to=i,
+                    from_=phone_no,
+                    url=URL
+                )
+                print(call.sid, datetime.datetime.now())
+            time.sleep(interval)
+        except:
+            print(traceback.format_exc())
 
-with open('groups/' + GROUP + '.txt') as f:
-    text = f.read()
-call_list = text.split('\n')
-for i in call_list:
-    call = client.calls.create(
-        to=i,
-        from_="+1XXXXXXXXXX",
-        url=URL
-    )
-    print(call.sid)
+
+if __name__ == '__main__':
+    with open('url.txt','r') as g:
+        URL = g.read()
+    
+    GROUP = input('Group: ')
+    INTERVAL = int(input('Interval (s): '))
+    TIMES = int(input('Times (s):'))
+
+    with open('groups/' + GROUP + '.txt') as f:
+        text = f.read()
+    call_list = text.split('\n')
+
+    call_them(time.time(), call_list, interval=INTERVAL, times=TIMES)
